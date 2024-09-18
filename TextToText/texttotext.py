04f7889fe05029660d1032abbd9005615f92bb05
Jatin Mayekar <44420409+jatinmayekar@users.noreply.github.com>
2024-09-18 18:52:01 -0400

    add open ai chat feature

--- TextToText/texttotext.py
+++ TextToText/texttotext.py
@@ -10,49 +10,65 @@ bCheckAPIKey = False
 bReadyToChat = False
 apiKey = ""
 
-if "bCheckAPIKey" not in st.session_state:
-    st.session_state.bCheckAPIKey = False
+if "bCheckApiKey" not in st.session_state:
+    st.session_state.bCheckApiKey = False
+
+if "modelName" not in st.session_state:
+    st.session_state.modelName = ""
+
+if "maxTokens" not in st.session_state:
+    st.session_state.maxTokens = 0
 
 st.title("T1: Text to Text")
 st.text("By Jatin Mayekar")
 
 tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(['OpenAI', 'Anthropic', 'Meta AI', 'Mistral', 'Hugging Face', 'Groq X'])
 
-def checkAPIKey(key):
+@st.cache_data
+def checkApiKey(apiKey):
     try:
+        client = OpenAI(api_key=apiKey)
         response = client.chat.completions.create(
             messages=[{
                         "role": "user",
                         "content": "Say this is a test",
                     }],
-            model="gpt-4o-mini",
+            model="gpt-4o-mini"
         )
         print(response)
         return True
     except openai.AuthenticationError as e:
         st.error("Invalid API key. You can find your API key at https://platform.openai.com/account/api-keys")
         return False
+    
+def getOpenAiResponse(prompt):
+        response = st.session_state.client.chat.completions.create(
+            messages=[{
+                        "role": "user",
+                        "content": prompt,
+                    }],
+            model=st.session_state.modelName,
+            max_tokens=st.session_state.maxTokens
+        )
+        print(response)
+        return response.choices[0].message.content
 
+# OpenAI
 with tab1:
     apiKey = st.text_input(label="Enter your API key", type='password', placeholder="sk-...")
     if apiKey != "":
-        client = OpenAI(api_key=apiKey)
-        bCheckAPIKey = checkAPIKey(apiKey)
+        st.session_state.bCheckApiKey = checkApiKey(apiKey)
         
-    # with st.form("OpenAI"):
-    #     if bCheckAPIKey:
-    #         st.write("Choose the options and submit to start chatting!")
-    modelName = st.selectbox(label="Select the model name", options=["gpt4o-mini", "gpt-4o"], disabled=not bCheckAPIKey)
-    maxTokens = st.number_input(label="Enter the max output text length", value=1024, disabled=not bCheckAPIKey, min_value=0, max_value=10000000)
+    st.session_state.modelName = st.selectbox(label="Select the model name", options=["gpt-4o-mini", "gpt-4o"], disabled=not st.session_state.bCheckApiKey)
+    st.session_state.maxTokens = st.number_input(label="Enter the max output text length", value=1024, disabled=not st.session_state.bCheckApiKey, min_value=0, max_value=10000000)
 
-        # bSubmitted = st.form_submit_button(label="Submit", disabled=not bCheckAPIKey)
-        # if bSubmitted:
-        #     bReadyToChat = True
-        #     st.write("Ready to chat!")
+    if st.session_state.bCheckApiKey:
+        if "client" not in st.session_state:
+            st.session_state.client = OpenAI(api_key=apiKey)
 
-prompt = st.chat_input("Hello... how can I help you?", disabled= not bCheckAPIKey)
+prompt = st.chat_input("Hello... how can I help you?", disabled= not st.session_state.bCheckApiKey)
 if prompt:
     with st.chat_message(name="user"):
         st.write(prompt)
     with st.chat_message(name="assistant"):
-        st.write("hello")
+        st.write(getOpenAiResponse(prompt))
