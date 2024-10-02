b09648377245bd2dcb1fe7e21cd7fbfee7ff1838
Jatin Mayekar <44420409+jatinmayekar@users.noreply.github.com>
2024-10-01 23:33:19 -0400

    Add new sidebar with radio choice for platform choice and settings popover and chat stats as a toggle feature and new chat when platform choice is changed and only chat interface as main interface

--- TextToText/texttotext.py
+++ TextToText/texttotext.py
@@ -11,12 +11,16 @@ bCheckAPIKey = False
 bReadyToChat = False
 apiKey = ""
 assistantResponse = ""
+bPlatformSwitch = False
 
 if "bCheckApiKey" not in st.session_state:
     st.session_state.bCheckApiKey = False
 
-if "tabIndex" not in st.session_state:
-    st.session_state.tabIndex = 0
+if "platformChoice" not in st.session_state:
+    st.session_state.platformChoice = None
+
+if "oldPlatformChoice" not in st.session_state:
+    st.session_state.oldPlatformChoice = None
 
 if "modelName" not in st.session_state:
     st.session_state.modelName = ""
@@ -37,11 +41,11 @@ st.title("T1: Text to Text")
 st.text("By Jatin Mayekar")
 
 #tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(['OpenAI', 'Anthropic', 'Meta AI', 'Mistral', 'Hugging Face', 'Groq X'])
-tab1, tab2 = st.tabs(['OpenAI', 'Anthropic'])
+#tab1, tab2 = st.tabs(['OpenAI', 'Anthropic'])
 
 @st.cache_data
 def checkApiKey(apiKey):
-    if st.session_state.tabIndex == 1:
+    if st.session_state.platformChoice == "OpenAI":
         print("OpenAI")
         try:
             client = OpenAI(api_key=apiKey)
@@ -57,7 +61,7 @@ def checkApiKey(apiKey):
         except openai.AuthenticationError as e:
             st.error("Invalid API key. You can find your API key at https://platform.openai.com/account/api-keys")
             return False
-    elif st.session_state.tabIndex == 2:
+    elif st.session_state.platformChoice == "Anthropic":
         print("Anthropic")
         try:
             client = anthropic.Anthropic(api_key=apiKey)
@@ -110,7 +114,7 @@ def checkApiKey(apiKey):
         print("Wrong tabIndex: ", st.session_state.tabIndex)
     
 def getOpenAiResponse(prompt):
-        response = st.session_state.client.chat.completions.create(
+        response = st.session_state.clientOpenAI.chat.completions.create(
             messages=[{
                         "role": "user",
                         "content": prompt,
@@ -122,7 +126,7 @@ def getOpenAiResponse(prompt):
         return response
 
 def getAnthropicResponse(prompt):
-        response = st.session_state.client.messages.create(
+        response = st.session_state.clientAnthropic.messages.create(
                 model=st.session_state.modelName,
                 max_tokens=st.session_state.maxTokens,
                 temperature=st.session_state.temperature,
@@ -139,114 +143,136 @@ def getAnthropicResponse(prompt):
         print(response)
         return response
 
-# OpenAI
-with tab1:
-    st.session_state.tabIndex = 1
-    apiKey = st.text_input(label="Enter your API key", type='password', placeholder="sk-proj...")
-    st.write("Find your API key at https://platform.openai.com/account/api-keys")
-    if apiKey != "":
-        st.session_state.bCheckApiKey = checkApiKey(apiKey)
-        
-    st.session_state.modelName = st.selectbox(label="Select the model name", options=["gpt-4o-mini", "gpt-4o", "gpt-4-turbo", "o1-mini", "o1-preview"], disabled=not st.session_state.bCheckApiKey)
-    st.session_state.maxTokens = st.number_input(label="Enter the max output text length", value=1024, disabled=not st.session_state.bCheckApiKey, min_value=0, max_value=10000000)
-    # take number of words as inputs rather than tokens - more intuitive than tokens. 1 token = 0.75 words
-    # source: https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
-    
-    if st.session_state.bCheckApiKey:
-        if "client" not in st.session_state:
-            st.session_state.client = OpenAI(api_key=apiKey)
+with st.sidebar:
+    st.session_state.platformChoice = st.radio(
+        "Platform Choice",
+        ["OpenAI", "Anthropic"]
+    )
 
-# Anthropic
-with tab2:
-    st.session_state.tabIndex = 2
-    apiKey = st.text_input(label="Enter your API key", type='password', placeholder="sk-ant...")
-    st.write("Find your API key at https://console.anthropic.com/settings/keys")
-    if apiKey != "":
-        st.session_state.bCheckApiKey = checkApiKey(apiKey)
-        
-    st.session_state.modelName = st.selectbox(label="Model", options=["claude-3-5-sonnet-20240620", "claude-3-opus-20240229", "claude-3-haiku-20240307", "claude-3-sonnet-20240229"], disabled=not st.session_state.bCheckApiKey)
-    st.session_state.temperature = st.number_input(label="Temperature (Higher for creative responses, lower for more predictable responses)", value=0, disabled=not st.session_state.bCheckApiKey, min_value=0, max_value=1)
-    st.session_state.maxTokens = st.number_input(label="Max output text length", value=1024, disabled=not st.session_state.bCheckApiKey, min_value=0, max_value=10000000)
-    # take nuber of words as inputs rather than tokens - more intuitive than tokens
+    # OpenAI
+    if st.session_state.platformChoice == "OpenAI":
+        if st.session_state.oldPlatformChoice != st.session_state.platformChoice:
+            bPlatformSwitch = True
+            st.session_state.oldPlatformChoice = st.session_state.platformChoice
+            st.session_state.bCheckApiKey =  False
+            st.session_state.messages = []
 
-    if st.session_state.bCheckApiKey:
-        if "client" not in st.session_state:
-            st.session_state.client = anthropic.Anthropic(api_key=apiKey)
+        with st.popover("Settings"):
+            apiKey = st.text_input(label="Enter your API key", type='password', placeholder="sk-proj...")
+            st.write("Find your API key at https://platform.openai.com/account/api-keys")
+            if apiKey != "":
+                st.session_state.bCheckApiKey = checkApiKey(apiKey)
+                
+            st.session_state.modelName = st.selectbox(label="Select the model name", options=["gpt-4o-mini", "gpt-4o", "gpt-4-turbo", "o1-mini", "o1-preview"], disabled=not st.session_state.bCheckApiKey)
+            st.session_state.maxTokens = st.number_input(label="Enter the max output text length", value=1024, disabled=not st.session_state.bCheckApiKey, min_value=0, max_value=10000000)
+            # take number of words as inputs rather than tokens - more intuitive than tokens. 1 token = 0.75 words
+            # source: https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
+            
+            if st.session_state.bCheckApiKey:
+                if "client" not in st.session_state:
+                    st.session_state.clientOpenAI = OpenAI(api_key=apiKey)
 
-    for message in st.session_state.messages:
-        with st.chat_message(name=message["role"]):
-            st.write(message["content"])
+    # Anthropic
+    if st.session_state.platformChoice == "Anthropic":
+        if st.session_state.oldPlatformChoice != st.session_state.platformChoice:
+            bPlatformSwitch = True
+            st.session_state.oldPlatformChoice = st.session_state.platformChoice
+            st.session_state.bCheckApiKey =  False
+            st.session_state.messages = []
 
-    input = st.chat_input("Hello... how can I help you?", disabled= not st.session_state.bCheckApiKey)
-    if input:
-        with st.chat_message(name="user"):
-            st.write(input)
-        with st.chat_message(name="assistant"):
-            if st.session_state.tabIndex == 1:
-                assistantResponse = getOpenAiResponse(input)
-                output = assistantResponse.choices[0].message.content
-            elif st.session_state.tabIndex == 2:
-                assistantResponse = getAnthropicResponse(input)
-                output = assistantResponse.content[0].text
-            st.write(output)
+        with st.popover("Settings"):
+            apiKey = st.text_input(label="Enter your API key", type='password', placeholder="sk-ant...")
+            st.write("Find your API key at https://console.anthropic.com/settings/keys")
+            if apiKey != "":
+                st.session_state.bCheckApiKey = checkApiKey(apiKey)
+                
+            st.session_state.modelName = st.selectbox(label="Model", options=["claude-3-5-sonnet-20240620", "claude-3-opus-20240229", "claude-3-haiku-20240307", "claude-3-sonnet-20240229"], disabled=not st.session_state.bCheckApiKey)
+            st.session_state.temperature = st.number_input(label="Temperature (Higher for creative responses, lower for more predictable responses)", value=0, disabled=not st.session_state.bCheckApiKey, min_value=0, max_value=1)
+            st.session_state.maxTokens = st.number_input(label="Max output text length", value=1024, disabled=not st.session_state.bCheckApiKey, min_value=0, max_value=10000000)
+            # take number of words as inputs rather than tokens - more intuitive than tokens
 
-        st.session_state.messages.append({"role": "user", "content":input})
-        st.session_state.messages.append({"role": "assistant", "content":output})
+            if st.session_state.bCheckApiKey:
+                if "client" not in st.session_state:
+                    st.session_state.clientAnthropic = anthropic.Anthropic(api_key=apiKey)
 
-        if st.session_state.tabIndex == 1:
-            if st.session_state.modelName=="gpt-4o-mini":
-                cost_per_prompt_token = 0.00500
-                cost_per_completion_token = 0.01500
-            elif st.session_state.modelName=="gpt-4o":
-                cost_per_prompt_token = 0.000150
-                cost_per_completion_token = 0.000600
-            elif st.session_state.modelName=="gpt-4-turbo":
-                cost_per_prompt_token = 0.0100
-                cost_per_completion_token = 0.0300
-            elif st.session_state.modelName=="o1-mini":
-                cost_per_prompt_token = 0.003
-                cost_per_completion_token = 0.012
-            elif st.session_state.modelName=="o1-preview":
-                cost_per_prompt_token = 0.015
-                cost_per_completion_token = 0.060
-        elif st.session_state.tabIndex == 2:
-            if st.session_state.modelName=="claude-3-5-sonnet-20240620":
-                cost_per_input_token = 0.003
-                cost_per_output_token = 0.015
-            elif st.session_state.modelName=="claude-3-opus-20240229":
-                cost_per_input_token = 0.015
-                cost_per_output_token = 0.075
-            elif st.session_state.modelName=="claude-3-sonnet-20240229":
-                cost_per_input_token = 0.003
-                cost_per_output_token = 0.015
-            elif st.session_state.modelName=="claude-3-haiku-20240307":
-                cost_per_input_token = 0.00025
-                cost_per_output_token = 0.00125
+    bChatStats = st.toggle("Show chat statistics")
 
+for message in st.session_state.messages:
+    with st.chat_message(name=message["role"]):
+        st.write(message["content"])
+
+input = st.chat_input("Hello... how can I help you?", disabled= not st.session_state.bCheckApiKey)
+if input:
+    with st.chat_message(name="user"):
+        st.write(input)
+    with st.chat_message(name="assistant"):
+        if st.session_state.platformChoice == "OpenAI":
+            assistantResponse = getOpenAiResponse(input)
+            output = assistantResponse.choices[0].message.content
+        elif st.session_state.platformChoice == "Anthropic":
+            assistantResponse = getAnthropicResponse(input)
+            output = assistantResponse.content[0].text
+        st.write(output)
+
+    st.session_state.messages.append({"role": "user", "content":input})
+    st.session_state.messages.append({"role": "assistant", "content":output})
+
+    if st.session_state.platformChoice == "OpenAI":
+        if st.session_state.modelName=="gpt-4o-mini":
+            cost_per_prompt_token = 0.00500
+            cost_per_completion_token = 0.01500
+        elif st.session_state.modelName=="gpt-4o":
+            cost_per_prompt_token = 0.000150
+            cost_per_completion_token = 0.000600
+        elif st.session_state.modelName=="gpt-4-turbo":
+            cost_per_prompt_token = 0.0100
+            cost_per_completion_token = 0.0300
+        elif st.session_state.modelName=="o1-mini":
+            cost_per_prompt_token = 0.003
+            cost_per_completion_token = 0.012
+        elif st.session_state.modelName=="o1-preview":
+            cost_per_prompt_token = 0.015
+            cost_per_completion_token = 0.060
+    elif st.session_state.platformChoice == "Anthropic":
+        if st.session_state.modelName=="claude-3-5-sonnet-20240620":
+            cost_per_input_token = 0.003
+            cost_per_output_token = 0.015
+        elif st.session_state.modelName=="claude-3-opus-20240229":
+            cost_per_input_token = 0.015
+            cost_per_output_token = 0.075
+        elif st.session_state.modelName=="claude-3-sonnet-20240229":
+            cost_per_input_token = 0.003
+            cost_per_output_token = 0.015
+        elif st.session_state.modelName=="claude-3-haiku-20240307":
+            cost_per_input_token = 0.00025
+            cost_per_output_token = 0.00125
+
+    with st.sidebar:
         with st.expander("Chat Stats"):
-            if st.session_state.tabIndex == 1:
-                st.write("Chat ID: ", assistantResponse.id)
-                st.write("Model: ", assistantResponse.model)
-                st.write("Prompt (input) tokens: ", assistantResponse.usage.prompt_tokens)
-                st.write("Completion (output) tokens: ", assistantResponse.usage.completion_tokens)
-                st.write("Reasoning tokens: ", assistantResponse.usage.completion_tokens_details['reasoning_tokens'])
-                st.write("Finish reason: ", assistantResponse.choices[0].finish_reason)
-                st.write("Function Calls: ", assistantResponse.choices[0].message.function_call)
-                st.write("Tool Calls: ", assistantResponse.choices[0].message.tool_calls)
-                currentCost = (assistantResponse.usage.prompt_tokens*cost_per_prompt_token/1000) + (assistantResponse.usage.completion_tokens*cost_per_completion_token/1000)
-                st.write("Cost: $ ", currentCost)
-                st.session_state.totalCost = st.session_state.totalCost + currentCost
-                st.write("Total Cost: $ ", st.session_state.totalCost)
-                st.write("See your total api usage here: https://platform.openai.com/organization/usage")
-            elif st.session_state.tabIndex == 2:
-                st.write("Chat ID: ", assistantResponse.id)
-                st.write("Model: ", assistantResponse.model)
-                st.write("Prompt (input) tokens: ", assistantResponse.usage.input_tokens)
-                st.write("Completion (output) tokens: ", assistantResponse.usage.output_tokens)
-                st.write("Finish/Stop reason: ", assistantResponse.stop_reason)
-                st.write("Stop sequence: ", assistantResponse.stop_sequence)
-                currentCost = (assistantResponse.usage.input_tokens*cost_per_input_token/1000) + (assistantResponse.usage.output_tokens*cost_per_output_token/1000)
-                st.write("Cost: $ ", currentCost)
-                st.session_state.totalCost = st.session_state.totalCost + currentCost
-                st.write("Total Cost: $ ", st.session_state.totalCost)
-                st.write("See your total api usage here: https://console.anthropic.com/settings/usage")
+            if bChatStats:
+                if st.session_state.platformChoice == "OpenAI":
+                    st.write("Chat ID: ", assistantResponse.id)
+                    st.write("Model: ", assistantResponse.model)
+                    st.write("Prompt (input) tokens: ", assistantResponse.usage.prompt_tokens)
+                    st.write("Completion (output) tokens: ", assistantResponse.usage.completion_tokens)
+                    st.write("Reasoning tokens: ", assistantResponse.usage.completion_tokens_details['reasoning_tokens'])
+                    st.write("Finish reason: ", assistantResponse.choices[0].finish_reason)
+                    st.write("Function Calls: ", assistantResponse.choices[0].message.function_call)
+                    st.write("Tool Calls: ", assistantResponse.choices[0].message.tool_calls)
+                    currentCost = (assistantResponse.usage.prompt_tokens*cost_per_prompt_token/1000) + (assistantResponse.usage.completion_tokens*cost_per_completion_token/1000)
+                    st.write("Cost: $ ", currentCost)
+                    st.session_state.totalCost = st.session_state.totalCost + currentCost
+                    st.write("Total Cost: $ ", st.session_state.totalCost)
+                    st.write("See your total api usage here: https://platform.openai.com/organization/usage")
+                elif st.session_state.platformChoice == "Anthropic":
+                    st.write("Chat ID: ", assistantResponse.id)
+                    st.write("Model: ", assistantResponse.model)
+                    st.write("Prompt (input) tokens: ", assistantResponse.usage.input_tokens)
+                    st.write("Completion (output) tokens: ", assistantResponse.usage.output_tokens)
+                    st.write("Finish/Stop reason: ", assistantResponse.stop_reason)
+                    st.write("Stop sequence: ", assistantResponse.stop_sequence)
+                    currentCost = (assistantResponse.usage.input_tokens*cost_per_input_token/1000) + (assistantResponse.usage.output_tokens*cost_per_output_token/1000)
+                    st.write("Cost: $ ", currentCost)
+                    st.session_state.totalCost = st.session_state.totalCost + currentCost
+                    st.write("Total Cost: $ ", st.session_state.totalCost)
+                    st.write("See your total api usage here: https://console.anthropic.com/settings/usage")
